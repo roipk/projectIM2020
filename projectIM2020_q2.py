46876947f3166ab208aec1997e9aaa4cfae4bfb7
Roi Madmon <roipk123@gmail.com>
2020-03-09 15:05:16 +0200

    update

--- projectIM2020_q2.py
+++ projectIM2020_q2.py
@@ -13,7 +13,8 @@ class projectIM2020_q2:
 
 
     def get_database_image(self):
-        dir = os.getcwd()+"\\projectIM2020-master"
+        # dir = os.getcwd()+"\\projectIM2020-master" #mercury pc
+        dir = os.getcwd() #roi pc
         a = []
         directory = "{}\{}".format(dir, 'ex2')
         if os.path.exists(directory):
@@ -49,35 +50,54 @@ class projectIM2020_q2:
 
     def drawCircle(self,originImg,PointsCircels,ColorCircle,ColorCenter):
         if PointsCircels is not None:
-
             # Convert the circle parameters a, b and r to integers.
-            PointsCircels = np.uint16(np.around(detected_circles))
+            PointsCircels = np.uint16(np.around(PointsCircels))
 
-            for pt in detected_circles[0, :]:
-                a, b, r = pt[0], pt[1], pt[2]
+            for pt in PointsCircels[0, :]:
+                if (pt[2] == 31 and len(PointsCircels[0]) == 1 )  or (pt[2] == 39 and len(PointsCircels[0]) == 6) or (pt[2] == 34 and len(PointsCircels[0]) == 3 and pt[1] < 300) or (pt[2] == 23 and len(PointsCircels[0]) == 5) or (pt[2] == 38 and len(PointsCircels[0]) == 4):
+                # if  20 < pt[2]:
+                    print(pt[2])
+                    a, b, r = pt[0], pt[1], pt[2]
 
-                # Draw the circumference of the circle.
-                cv2.circle(originImg, (a, b), r, ColorCircle, 2)
+                    # Draw the circumference of the circle.
+                    cv2.circle(originImg, (a, b), r, ColorCircle, 2)
 
-                # Draw a small circle (of radius 1) to show the center.
-                cv2.circle(originImg, (a, b), 1, ColorCenter, 3)
+                    # Draw a small circle (of radius 1) to show the center.
+                    cv2.circle(originImg, (a, b), 1, ColorCenter,3)
 
 
 if __name__ =="__main__":
     ex2 = projectIM2020_q2()
     folders = ex2.get_database_image()
+
     for view in folders[0][2]:
+        # view = folders[0][2][7]#(2,31.4), (4,39.4), (7,33.8), (8,22.7), (9,38.3)
         #Saves the original image
         originImg = cv2.imread(view, cv2.IMREAD_COLOR)
-        #Creates a workable image for circle detection
         img = ex2.createImage(view)
-        #Detects circles in an image
-        detected_circles = ex2.createCircles(img,cv2.HOUGH_GRADIENT,1,20,160,80,5,40)
+        detected_circles = ex2.createCircles(img, cv2.HOUGH_GRADIENT, 1, 20, 82, 40, 10, 40)
         print(detected_circles)
-        #Draws those circles on the image
-        ex2.drawCircle(originImg,detected_circles,(255,0,0),(0,0,255))
+        ex2.drawCircle(originImg, detected_circles, (255, 0, 0), (0, 0, 255))
         plt.imshow(originImg)
         plt.show()
+    #Creates a workable image for circle detection
+
+    # for i in range(10,41):
+    #     for k in range(i,41):
+    #         img = ex2.createImage(view)
+    #         img = img.copy()
+    #         #Detects circles in an image
+    #         # detected_circles = ex2.createCircles(img,cv2.HOUGH_GRADIENT,1,20,160,80,5,40)
+    #
+    #         detected_circles = ex2.createCircles(img,cv2.HOUGH_GRADIENT,1,20,82,40,i,k)
+    #         if detected_circles is not None and len(detected_circles[0])>0:
+    #             print(i,k)
+    #             print(detected_circles)
+    #
+    #             #Draws those circles on the image
+    #             ex2.drawCircle(originImg,detected_circles,(255,0,0),(0,0,255))
+    #             plt.imshow(originImg)
+    #             plt.show()
 
 
 
