4c171a5f0f833bb3742547b5258f9fcd5ef49aae
Roi Madmon <roipk123@gmail.com>
2020-03-10 16:42:38 +0200

    q3 need some fix

--- projectIM2020_q2.py
+++ projectIM2020_q2.py
@@ -13,13 +13,21 @@ class projectIM2020_q2:
 
 
     def get_database_image(self):
-        dir = os.getcwd()+"\\projectIM2020-master"
+        # dir = os.getcwd()+"\\projectIM2020-master"
+        dir = os.getcwd()
         a = []
         directory = "{}\{}".format(dir, 'ex2')
         if os.path.exists(directory):
             folders = list(os.walk(directory))
             os.chdir(directory)
             return  folders
+
+    def createImage(self, path):
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        # Convert to grayscale.
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_blurred = cv2.blur(gray, (3, 3))
+        return gray_blurred
 
     def find_circle(self, img):
         edges = cv2.Canny(img, 150, 100, apertureSize=3)
@@ -37,12 +45,7 @@ class projectIM2020_q2:
 
         return img
 
-    def createImage(self, path):
-        img = cv2.imread(path, cv2.IMREAD_COLOR)
-        # Convert to grayscale.
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray_blurred = cv2.blur(gray, (3, 3))
-        return gray_blurred
+
 
     def createCircles(self,img , cv,resolution,dr,param1,param2,minRadius,maxRadius):
         return cv2.HoughCircles(img, cv, resolution, dr, param1=param1, param2=param2, minRadius=minRadius,maxRadius=maxRadius)
@@ -53,7 +56,7 @@ class projectIM2020_q2:
             # Convert the circle parameters a, b and r to integers.
             PointsCircels = np.uint16(np.around(detected_circles))
 
-            for pt in detected_circles[0, :]:
+            for pt in PointsCircels[0, :]:
                 a, b, r = pt[0], pt[1], pt[2]
 
                 # Draw the circumference of the circle.
@@ -74,32 +77,9 @@ if __name__ =="__main__":
         #Detects circles in an image
         detected_circles = ex2.createCircles(img, cv2.HOUGH_GRADIENT, 1, 20, 82, 40, 20, 35)
         # Draws those circles on the image
-        print(detected_circles)
         if detected_circles is not None and len(detected_circles[0]) == 1:
             ex2.drawCircle(originImg, detected_circles, (255, 0, 0), (0, 0, 255))
         plt.imshow(originImg)
         plt.show()
 
 
-
-
-        # detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=160, param2=80, minRadius=5,maxRadius=40)
-
-
-
-
-
-
-
-
-        # img = cv2.imread(view)
-        # # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # # img1 = cv2.medianBlur(img, 5)
-        # img_edge = ex2.find_edges(img)
-        #
-        # img_circles = ex2.find_circle(img_edge)
-        # # # imgs = img + img_circles
-        # # # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # # # imgs = gray + iom
-        # plt.imshow(img_circles, cmap='gray'), plt.xticks([]), plt.yticks([])
-        # plt.show()
